4fca43a09c2d3328d30a550e4c49198f86d64df5
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-15 11:30:38 +0300

    last commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -15,15 +15,56 @@ class ViewController: UIViewController {
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
     
-    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        setValue(for: redLabel, greenLabel, blueLabel)
+        
+    }
+    
+   // MARK: - @IBAction
+    @IBAction func setValue(for sender: UISlider) {
+        switch sender {
+        case redSlider:
+            setValue(for: redLabel)
+        case greenSlider:
+            setValue(for: greenLabel)
+        default:
+            setValue(for: blueLabel)
+            
+        }
+    }
+    
+    // MARK: - Private methods
+    private func setValue(for labels: UILabel...) {
+        setColor()
+        
+        for label in labels {
+            switch label {
+            case redLabel:
+                label.text = string(redSlider)
+            case greenLabel:
+                label.text = string(greenSlider)
+            default:
+                label.text = string(blueSlider)
+            }
+        }
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func string(_ slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
 
